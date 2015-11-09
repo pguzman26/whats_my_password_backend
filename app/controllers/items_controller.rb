3fcd39b4dad991cdc5549c2fe6479a8b73c22ce9
gmankd <pabloguzman26@yahoo.com>
2015-11-09 15:02:48 -0500

    Added items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,20 @@
+#
 class ItemsController < ApplicationController
   def index
     @items = Item.for_list(params[:list_id])
+
+    respond_to do |format|
+      format.json { render json: @items }
+    end
+  end
+
+  def create
+    @item = Item.create(params[:item])
+    redirect_to list_items_url
+  end
+
+  def destroy
+    Item.find_by_list_id_and_id(params[:list_id], params[:id]).destroy
+    redirect_to list_items_url
+  end
+end
