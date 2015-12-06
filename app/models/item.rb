8fc1255dbf8d022897e811db4ab937d0a76eda14
gmankd <pabloguzman26@yahoo.com>
2015-12-06 11:56:09 -0500

    Changed items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,12 @@
 #
 class Item < ActiveRecord::Base
-<<<<<<< HEAD
   # attr_accessible specifies a white list of
   # model attributes that can be set via mass assignment.
   attr_accessible :list_id, :name
-=======
   belongs_to :user
   # attr_accessible specifies a white list of
   # model attributes that can be set via mass assignment.
-  # attr_accessible :list_id, :name
->>>>>>> model
+  # attr_accessible :list_id, :namemodel
 
   # is a shortcut to all default validators and
   # any custom validator classes ending in
@@ -17,11 +14,10 @@ class Item < ActiveRecord::Base
   # validators can be overridden inside specific
   # classes by creating custom validator
   # classes in their place such as PresenceValidator.
-<<<<<<< HEAD
+
   validates :name, :list_id, presence: true
-=======
-  # validates :name, :list_id, presence: true
->>>>>>> model
+
+  # validates :name, :list_id, presence: truemodel
 
   def self.for_list(list_id)
     where(list_id: list_id)
